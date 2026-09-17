7675fc4e126ae7976978852fa1c62e832b84a122
Davito Ezra <davitoezra@gmail.com>
2026-09-17 21:07:17 +0700

    Update Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,33 +1,3 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import fs from "fs";
-
-export default defineConfig({
-    plugins: [react()],
-
-    server: {
-        host: "0.0.0.0",
-
-        https: {
-            key: fs.readFileSync("./localhost+2-key.pem"),
-            cert: fs.readFileSync("./localhost+2.pem"),
-        },
-
-        proxy: {
-            "/api": {
-                target: "http://127.0.0.1:8000",
-                changeOrigin: true,
-                secure: false,
-            },
-        },
-    },
-
-    preview: {
-        host: "0.0.0.0",
-
-        https: {
-            key: fs.readFileSync("./localhost+2-key.pem"),
-            cert: fs.readFileSync("./localhost+2.pem"),
-        },
-    },
-});
+import { defineConfig } from "vite"; 
+import react from "@vitejs/plugin-react"; 
+export default defineConfig({ plugins: [react()], });
